5af373abac0d3481deea391780a46f795c66b4ea
Vincentius Park <Vincentius@Vincentiuss-MacBook-Pro.local>
2015-04-25 15:42:40 +0700

    Initial Commit

---
+++ c++/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  c++
+//
+//  Created by Vincentius Park on 4/25/15.
+//  Copyright (c) 2015 Vincentius Park. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
